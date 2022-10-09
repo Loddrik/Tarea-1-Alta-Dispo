85dfb2997395a0c1be5b4331118620a8aa4640f5
tanaxer01 <jpbrione@gmail.com>
2022-10-09 13:28:13 -0300

    fixed

--- API/src/index.js
+++ API/src/index.js
@@ -18,7 +18,4 @@ app.get("/", async (req, res) => { res.status(200).send({ message: "olakace"})})
 app.use("/user", users);
 app.use("/recipe", recipes);
 
-app.listen(3001, "localhost", () => {
-    console.log(`API corriendo (mela) en el puorto 😈💦  : 8080.`);
-    console.log(process.env.DB_HOST)
-})
+app.listen(3001, () => console.log(`API corriendo (mela) en el puorto 😈💦  : 8080.`) );
